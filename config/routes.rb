0ce8fa510b4c20b0fbd0c9d6f968db38ad01a7dd
lena-don <lena08don@gmail.com>
2023-10-18 00:22:34 +0300

    исправление ошибки в комментариях

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
   get 'welcome/about'
+  get 'welcome/map'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
