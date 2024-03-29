7f1132296f3149c498b2e0c96c61e6d4016f6a46
lena-don <lena08don@gmail.com>
2024-03-29 14:30:43 +0300

    фильтры туториалов

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -1,31 +1,28 @@
 function tagsVisibility() {
-    const buttonItems = document.querySelector('.TagSection.Items')
-    const tagListItems = document.querySelector('.TagList.Items')
-    const arrowItems = document.querySelector('.IconArrowDown.Items')
-    const buttonTech = document.querySelector('.TagSection.Tech')
-    const tagListTech = document.querySelector('.TagList.Tech')
-    const arrowTech = document.querySelector('.IconArrowDown.Tech')
-    const buttonMat = document.querySelector('.TagSection.Mat')
-    const tagListMat = document.querySelector('.TagList.Mat')
-    const arrowMat = document.querySelector('.IconArrowDown.Mat')
+    const buttonItems = document.querySelector('.M_FilterSelect.Items')
+    const tagListItems = document.querySelector('.M_FilterSelectDropdown.Items')
+    const crossItems = document.querySelector('.Q_Icon.Cross.Items')
+    const buttonTech = document.querySelector('.M_FilterSelect.Tech')
+    const tagListTech = document.querySelector('.M_FilterSelectDropdown.Tech')
+    const crossTech = document.querySelector('.Q_Icon.Cross.Tech')
+    const buttonMat = document.querySelector('.M_FilterSelect.Mat')
+    const tagListMat = document.querySelector('.M_FilterSelectDropdown.Mat')
+    const crossMat = document.querySelector('.Q_Icon.Cross.Mat')
 
 
     buttonItems.addEventListener('click', (e) => {
         e.stopPropagation();
         tagListItems.classList.toggle('show')
-        arrowItems.classList.toggle('CD')
     })
 
     buttonTech.addEventListener('click', (e) => {
         e.stopPropagation();
         tagListTech.classList.toggle('show')
-        arrowTech.classList.toggle('CD')
     })
 
     buttonMat.addEventListener('click', (e) => {
         e.stopPropagation();
         tagListMat.classList.toggle('show')
-        arrowMat.classList.toggle('CD')
     })
 
     document.addEventListener( 'click', (e) => {
@@ -34,25 +31,19 @@ function tagsVisibility() {
         const ItagList = target == tagListItems || tagListItems.contains(target)
         const ItagButton = target == buttonItems
         const ItagListShow = tagListItems.classList.contains('show')
-        const Iarrow = arrowItems.classList.contains('CD')
 
         const TtagList = target == tagListTech || tagListTech.contains(target)
         const TtagButton = target == buttonTech
         const TtagListShow = tagListTech.classList.contains('show')
-        const Tarrow = arrowTech.classList.contains('CD')
 
         const MtagList = target == tagListMat || tagListMat.contains(target)
         const MtagButton = target == buttonMat
         const MtagListShow = tagListMat.classList.contains('show')
-        const Marrow = arrowMat.classList.contains('CD')
 
         if ( ( !ItagList && !ItagButton && ItagListShow && Iarrow ) || ( !TtagList && !TtagButton && TtagListShow && Tarrow) || ( !MtagList && !MtagButton && MtagListShow && Marrow) )  {
             tagListItems.classList.remove('show')
-            arrowItems.classList.remove('CD')
             tagListTech.classList.remove('show')
-            arrowTech.classList.remove('CD')
             tagListMat.classList.remove('show')
-            arrowMat.classList.remove('CD')
         }
     })
 }
@@ -110,7 +101,6 @@ function mobileMenuVisibility() {
 document.addEventListener('DOMContentLoaded', () => {
     if (document.body.classList.contains('tutorials_page')) {
         tagsVisibility()
-        addLike()
     }
 
     if (document.body.classList.contains('ideas_page')) {
